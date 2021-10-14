10503d6a8fc89a8114a3d08642cead488b265206
kh-min7 <rudgh2879@naver.com>
2021-10-14 23:55:06 +0900

    .

--- 2562.py
+++ 2562.py
@@ -1,4 +1,23 @@
+import sys
+
+sys.stdin = open("input.txt", "r")
+
+
 height = []
 for _ in range(9):
     height.append(int(input()))
 
+sum_height = sum(height)
+
+for i in range(len(height)):
+    for j in range(i+1, len(height)):
+        a, b = height[i], height[j]
+        if sum_height - a - b == 100:
+            height.remove(a)
+            height.remove(b)
+            break
+
+height.sort()
+
+for x in height:
+    print(x)
