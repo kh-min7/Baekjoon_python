a777167174f5b5dff2da00b485028a06f3eb2bf0
kh-min7 <rudgh2879@naver.com>
2021-11-04 23:33:06 +0900

    .

--- Greedy/2839.py
+++ Greedy/2839.py
@@ -1,4 +1,16 @@
-import sys
+n = int(input())
+F = n // 5
+n %= 5
+T = 0
+while F >= 0:
+    if n % 3 == 0:
+        T = n // 3
+        n = n % 3
+        break
+    F -= 1
+    n += 5
 
-sys.stdin = open("input.txt", "r")
-
+if n == 0:
+    print(F + T)
+else:
+    print(-1)
