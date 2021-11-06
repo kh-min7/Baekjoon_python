1cbbd82441f5dcc9ef0739d27efebd99d15fb69f
kh-min7 <rudgh2879@naver.com>
2021-11-06 23:04:02 +0900

    .

--- 2581.py
+++ 2581.py
@@ -5,3 +5,35 @@ sys.stdin = open("input.txt", "r")
 m = int(input())
 n = int(input())
 
+
+def primeNum(x):
+    cnt = 0
+
+    if x <= 1:
+        cnt = 0
+
+    elif x == 2:
+        cnt = 1
+
+    else:
+        for i in range(2, x):
+            if x % i == 0:
+                cnt = 0
+                break
+            else:
+                cnt = 1
+
+    return cnt
+
+
+nums = []
+
+for i in range(m, n + 1):
+    if primeNum(i) == 1:
+        nums.append(i)
+
+if len(nums) > 0:
+    print(sum(nums))
+    print(min(nums))
+else:
+    print(-1)
