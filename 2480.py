c38ad3393be8b494fbf3725a3de5a2ea91ad780e
kh-min7 <rudgh2879@naver.com>
2022-06-10 22:47:04 +0900

    .

--- 2480.py
+++ 2480.py
@@ -1,1 +1,17 @@
-2480.py
+a, b, c = map(int, input().split())
+
+if a == b == c:
+  ans = 10000 + a * 1000
+  print(ans)
+elif a == b and b != c:
+  ans = 1000 + a * 100
+  print(ans)
+elif b == c and a != b:
+  ans = 1000 + b * 100
+  print(ans)
+elif c == a and a != b:
+  ans = 1000 + a * 100
+  print(ans)
+else:
+  ans = max(a, b, c) * 100
+  print(ans)
---
+++ 2525.py
@@ -0,0 +1,18 @@
+a, b = map(int, input().split())
+c = int(input())
+
+def clock(a, b, c):
+  b += c
+  if b >= 60:
+    d = b // 60
+    e = b % 60
+  
+    b = e
+    a += d
+
+  if a >= 24:
+    a -= 24
+  
+  print(a, b)
+
+clock(a, b, c)
